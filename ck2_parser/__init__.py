b63434f3586178d3119c9613636ea2e61af08f37
Jamil Navarro <jamilnavarro@gmail.com>
2016-04-17 15:02:41 -0300

    Created Python package ck2_parser

---
+++ ck2_parser/__init__.py
@@ -0,0 +1,1 @@
+from .ck2_file_parser import ck2_parser
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(  name='ck2_parser',
+        version='0.1',
+        description='Utilities to parse CK2 save files',
+        url='https://github.com/jamilnavarro/ck2_parser',
+        author='Jamil Navarro',
+        author_email='jamilnavarro@gmail.com',
+        license='GPL',
+        packages=['ck2_parser'],
+        zip_safe=False)
